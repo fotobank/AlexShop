c127897277a69f0d0305002be90fdf22e880c891
Алексеев Юрий <aleksjurii@gmail.com>
2016-07-29 20:44:56 +0300

    29.07.16

---
+++ config/config.php
@@ -0,0 +1,74 @@
+;<? exit(); ?>
+
+license = "fbbcaeejjl nmjmqgmkrk mnrwrpvqq5 6444bbb6 abelgldggm mpqjosqnqr opw4t5355c 8bbffhfcga emjghnfiip ltkonwstty y6984e8fd6 gjaf9cecki jojqommril trmqs7r2s5 5a886dcb8c hgagdjgmlq kmmmlqjmqv v5xatcy67e 6hei6feifi gnihkmgjhk mplqsql4r2 t7yd6f2c5h aebigmighi gnemlqplpo o3o5r6u7va 185i8ibk7j ekimiieqln orkm "
+
+[database]
+
+;Сервер базы данных
+db_server = "localhost"
+
+;Пользователь базы данных
+db_user = "simpla"
+
+;Пароль к базе
+db_password = "278019"
+
+;Имя базы
+db_name = "simpla"
+
+;Префикс для таблиц
+db_prefix = s_;
+
+;Кодировка базы данных
+db_charset = UTF8;
+
+;Режим SQL
+db_sql_mode =;
+
+;Смещение часового пояса
+;db_timezone = +04:00;
+
+
+[php]
+error_reporting = E_ALL;
+php_charset = UTF8;
+php_locale_collate = ru_RU;
+php_locale_ctype = ru_RU;
+php_locale_monetary = ru_RU;
+php_locale_numeric = ru_RU;
+php_locale_time = ru_RU;
+;php_timezone = Europe/Moscow;
+
+logfile = admin/log/log.txt;
+
+[smarty]
+
+smarty_compile_check = true;
+smarty_caching = false;
+smarty_cache_lifetime = 0;
+smarty_debugging = false;
+smarty_html_minify = false;
+ 
+[images]
+;Использовать imagemagick для обработки изображений (вместо gd)
+use_imagick = true;
+
+;Директория оригиналов изображений
+original_images_dir = files/originals/;
+
+;Директория миниатюр
+resized_images_dir = files/products/;
+
+;Изображения категорий
+categories_images_dir = files/categories/;
+
+;Изображения брендов
+brands_images_dir = files/brands/;
+
+;Файл изображения с водяным знаком
+watermark_file = simpla/files/watermark/watermark.png;
+
+[files]
+
+;Директория хранения цифровых товаров
+downloads_dir = files/downloads/;
